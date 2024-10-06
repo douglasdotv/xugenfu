9032247368f21237ba6f0e400a91115f9fcc99e4
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-05 21:00:33 -0300

    server: add MongoDB connection
    - Installed mongoose and dotenv
    - Set up MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error)
+  }
+}
+mongoose.set('strictQuery', false)
+connectToDatabase()
 
 app.get('/', (_req, res) => {
   res.send('Hello, world!')
