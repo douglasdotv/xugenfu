521f673915830e0443e62d1d5294e600167d3858
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-05 21:32:40 -0300

    server: update /api route to return JSON status

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-  res.send('Hello, world!')
+  res.status(200).json({ message: 'API is running', status: 'OK' })
 })
 
 module.exports = router
