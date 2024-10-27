81a62c2f1873e4fb87cb4c7aa9d447c92de9b663
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-27 11:38:09 -0300

    server: add CORS middleware for client integration
    Installed cors and updated app.js to allow requests from http://localhost:5173 origin

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const apiRoutes = require('./routes/api')
 const usersRoutes = require('./routes/users')
 const connectToDatabase = require('./utils/db')
@@ -6,6 +7,11 @@ const connectToDatabase = require('./utils/db')
 const app = express()
 
 app.use(express.json())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+)
 
 connectToDatabase()
 
