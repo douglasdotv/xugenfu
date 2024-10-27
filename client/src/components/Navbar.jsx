0d5d0756d718caa03da5f01ddad4e3155c3333ff
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-27 12:54:00 -0300

    client: enhance navbar with conditional auth links and logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,30 @@
-import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../contexts/AuthContext'
 
 const Navbar = () => {
+  const { auth, logout } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/')
+  }
+
   return (
     <nav>
-      <Link to="/">Xugenfu</Link> | <Link to="/login">Log in</Link> {' | '}
-      <Link to="/signup">Sign up</Link>
+      <Link to="/">Xugenfu</Link> |{' '}
+      {auth ? (
+        <>
+          <Link to="/dashboard">Dashboard</Link> |{' '}
+          <span>Welcome, {auth.user.username}!</span>
+          <button onClick={handleLogout}>Log out</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Log in</Link> | <Link to="/signup">Sign up</Link>
+        </>
+      )}
     </nav>
   )
 }
