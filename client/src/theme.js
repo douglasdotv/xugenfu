cf073791121a87e7d8c5a0d88a4c75c09dede96c
Douglas Vieira da Silva <douglas16722@gmail.com>
2025-01-14 19:01:41 -0300

    client: improve layout and add custom scrollbar styles

--- client/src/theme.js
+++ client/src/theme.js
@@ -8,12 +8,33 @@ const theme = createTheme({
   components: {
     MuiCssBaseline: {
       styleOverrides: {
-        body: {
-          background:
-            'linear-gradient(135deg,rgb(220, 220, 226) 0%, #ffd9f4 100%)',
-          minHeight: '100vh',
+        html: {
+          width: '100%',
+          height: '100%',
           margin: 0,
           padding: 0,
+          overflowX: 'hidden',
+          overflowY: 'auto',
+        },
+        body: {
+          width: '100%',
+          height: '100%',
+          margin: 0,
+          padding: 0,
+          overflowX: 'hidden',
+          background:
+            'linear-gradient(135deg, rgb(220,220,226) 0%, #ffd9f4 100%)',
+        },
+        '::-webkit-scrollbar': {
+          width: '12px',
+        },
+        '::-webkit-scrollbar-track': {
+          backgroundColor: '#0A2A43',
+        },
+        '::-webkit-scrollbar-thumb': {
+          backgroundColor: '#ED8A14',
+          borderRadius: '6px',
+          border: '2px solid #0A2A43',
         },
       },
     },
