8513cde31917d50246a4ec7728d8b27f52952115
Douglas Vieira da Silva <douglas16722@gmail.com>
2025-01-14 12:08:07 -0300

    client: style bg

--- client/src/theme.js
+++ client/src/theme.js
@@ -2,11 +2,20 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#FF5722',
-    },
-    secondary: {
-      main: '#2196F3',
+    primary: { main: '#FF5722' },
+    secondary: { main: '#2196F3' },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background:
+            'linear-gradient(135deg,rgb(220, 220, 226) 0%, #ffd9f4 100%)',
+          minHeight: '100vh',
+          margin: 0,
+          padding: 0,
+        },
+      },
     },
   },
 });
